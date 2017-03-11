e39de937cdb42cee52c0bccae9444fe6c402e8c5
yangkaibo <yangkaiboo@163.com>
2017-03-11 17:21:53 +0800

    build android

--- Android.mk
+++ Android.mk
@@ -6,7 +6,22 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := dtex2
 
+LOCAL_C_INCLUDES  := \
+	${DTEX2_SRC_PATH}/include \
+	${DTEX2_SRC_PATH}/include/dtex2 \
+	${PS_SRC_PATH} \
+	${CU_SRC_PATH} \
+	${TASKS_SRC_PATH} \
+	${TEXPACK_SRC_PATH} \
+	${LOGGER_SRC_PATH} \
+	${GIMG_SRC_PATH} \
+	${UNIRENDER_SRC_PATH}/include \
+
 LOCAL_SRC_FILES := \
+	$(subst $(LOCAL_PATH)/,,$(shell find $(LOCAL_PATH)/source/ -name "*.cpp" -print)) \
+
+LOCAL_STATIC_LIBRARIES := \
+	tasks \
 
 include $(BUILD_STATIC_LIBRARY)
 
